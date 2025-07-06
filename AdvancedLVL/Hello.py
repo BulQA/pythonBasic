d19d778ef03704c5889afeb587ffba8831ffbd20
Nikolay Garipov <kolya.garipov.2000@mail.ru>
2025-07-06 21:46:44 +0300

    Знакомство с функиями PyCharm

--- AdvancedLVL/Hello.py
+++ AdvancedLVL/Hello.py
@@ -1,1 +1,30 @@
 print('Congratulations, you opened me in Windows Power Shell!!!')
+
+#Работа с дебагом
+
+# number = int(input('Any number:'))
+#
+# num_sum = 0
+#
+# while number != 0:
+#     last_num = number % 10
+#     num_sum += last_num
+#     number //= 10
+#
+# print('num', number , 'sum:', num_sum)
+
+#Дебаг с условием
+
+# def sum_factorials(num):
+#     factorial = 1
+#     total = 0
+#
+#     for i in range(1, num + 1):
+#         factorial *= i
+#         total += factorial
+#
+#     return total
+#
+# number = int(input('Number:'))
+# fac_sum = sum_factorials(number)
+# print('fac sum for', number, 'is', fac_sum)
