2b0cf1886d514c188232c795ef55d789b613d2eb
Nikolay Garipov <kolya.garipov.2000@mail.ru>
2025-07-09 23:09:34 +0300

    Работа с list

--- AdvancedLVL/practice_1_1.py
+++ AdvancedLVL/practice_1_1.py
@@ -27,3 +27,48 @@
 #
 # print('New list of books:', new_books_id)
 # print('Returned books:', returned)
+
+#2.2 Индексы. Работа с элементами списка
+
+# scores = [8, 5, 10, 7, 6]
+# print(scores)
+
+# +++++++++++++++++++++++++++++++++++++++++++#
+
+# new_scores = []
+# for score in new_scores:
+# 	if score == 5:
+# 		score *= 3
+# 	new_scores.append(score)
+#
+# print(new_scores)
+
+# +++++++++++++++++++++++++++++++++++++++++++#
+
+# scores = [8, 5, 10, 7, 6]
+# scores[1] *= 3
+
+# +++++++++++++++++++++++++++++++++++++++++++#
+
+# x = scores[4]
+# x += 10
+#
+# print(scores)
+
+# +++++++++++++++++++++++++++++++++++++++++++#
+
+monsters_count = int(input('Total monsters:'))
+mage_index = int(input('Caster top:'))
+
+monsters_dmg = []
+
+for monster in range(monsters_count):
+	print('DMG of', monster + 1 , 'monster:', end = ' ')
+	dmg = int(input())
+	monsters_dmg.append(dmg)
+
+for i in range(monsters_count):
+	if monsters_dmg[i] < 100 and i != mage_index - 1:
+		monsters_dmg[i] += monsters_dmg[mage_index - 1]
+
+print('Total DMG of monsters:', monsters_dmg)
