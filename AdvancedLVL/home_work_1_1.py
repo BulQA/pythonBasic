842d55d4a8d837773ae35c4ace8a4e585c53fea6
Nikolay Garipov <kolya.garipov.2000@mail.ru>
2025-07-22 22:00:21 +0300

    дз по спискам

--- AdvancedLVL/home_work_1_1.py
+++ AdvancedLVL/home_work_1_1.py
@@ -62,9 +62,44 @@
 #             if my_list[curr] < my_list[i_mn]:
 #                 my_list[curr], my_list[i_mn] = my_list[i_mn], my_list[curr]
 #
-#
-# nums = [4, 9, 7, 6, 3, 2]
+#nums = [4, 9, 7, 6, 3, 2]
 #
 # selection_sort(nums)
 #
 # print(nums)
+
+# `````````````````Дз```````````````````````#
+
+# nums = []
+# reverse_list = []
+# for i in range((len*(nums))-2, -1, -1):
+# 	    reverse_list.append(nums[i])
+
+# `````````````````Дз```````````````````````#
+
+# def is_palendrom(num_list):
+# 	reverse_list = []
+# 	for i_num in range(len(num_list)- 1, -1, -1):
+# 		reverse_list.append(num_list[i_num])
+# 	if num_list == reverse_list:
+# 		return True
+# 	else:
+# 		return False
+#
+# nums = [1, 2, 3, 4, 5]
+# new_nums = []
+# answer = []
+#
+# for i_nums in range(0, len(nums)):
+# 	for j_elem in range(i_nums, len(nums)):
+# 		new_nums.append(nums[j_elem])
+# 	if is_palendrom(new_nums):
+# 		for i_answer in range(0, i_nums):
+# 			answer.append(nums[i_answer])
+# 		answer.reverse()
+# 		break
+# 	new_nums = []
+#
+# print("Yo'r list:", nums)
+# print("Exist number's of polendrom:", len(answer))
+# print("List of exist digits:", answer)
