50b19b8119b10536b76dee5d86be2e4cfb18ad77
Nikolay Garipov <kolya.garipov.2000@mail.ru>
2025-08-14 21:19:48 +0300

    8.4 Передача изменяемых и неизменяемых данных в функцию

---
+++ AdvancedLVL/practice_1_3.py
@@ -0,0 +1,25 @@
+# 8.4 Передача изменяемых и неизменяемых данных в функцию
+
+# def try_to_change(some_list, num):
+#     for i_index, i_val in enumerate(some_list):
+#         some_list[i_index] += 10
+#     num += 10
+#
+#
+# my_list = [1, 2, 4, 3, 5]
+# number = 75
+#
+# try_to_change(my_list, number)
+# print(my_list, number)
+
+#+++++++++++++++++++++++++++++++++++++++++++#
+
+# int
+# float
+# str  # Неизменяемые(immutable) типы данных
+# tuple
+# bool
+#
+# list
+# dict  # Изменяемые(mutable) типы данных
+# set
