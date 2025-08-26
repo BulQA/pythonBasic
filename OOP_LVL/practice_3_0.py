28467ea6d87784a1b39b6c105a6d7b84a0bca88e
Nikolay Garipov <kolya.garipov.2000@mail.ru>
2025-08-26 21:45:24 +0300

    __init__

--- OOP_LVL/practice_3_0.py
+++ OOP_LVL/practice_3_0.py
@@ -7,7 +7,6 @@
 # 	'user_name':'Admin',
 # 	'password':'qwerty',
 # 	'is_banned': False
-}
 
 # +++++++++++++++++++++++++++++++++++++++++++#
 
@@ -54,7 +53,7 @@
 #
 # print(user_1.friends)
 
-# +++++++++++++++++++++++++++++++++++++++++++#
+#+++++++++++++++++++++++++++++++++++++++++++#
 
 # class Family:
 #     surname = 'Common Family'
@@ -94,3 +93,74 @@
 #     my_family.buy_house(10 ** 5, 10)
 #
 # my_family.info()
+
+# 11.4 Конструктор __init__ и работа с несколькими классами
+
+# class Employer:
+#
+#     def __init__(self, name, salary):
+#         self.name = name
+#         self.salary = salary
+#
+#     def print_info(self):
+#         print(
+#             'Name: {}\nSalary: {}'.format(self.name, self.salary)
+#         )
+#
+#
+# emp_1 = Employer('Bob', 900)
+# emp_2 = Employer('Shel', 1000)
+#
+# emp_1.print_info()
+# emp_2.print_info()
+
+#+++++++++++++++++++++++++++++++++++++++++++#
+
+# class Potato:
+#     state = {0: 'Отсутствует', 1: 'Росток', 2: 'Зелёная', 3: 'Зрелая'}
+#
+#     def __init__(self, index):
+#         self.index = index
+#         self.state = 0
+#
+#     def grow(self):
+#         if self.state < 3:
+#             self.state += 1
+#         self.print_state()
+#
+#     def is_ripe(self):
+#         if self.state == 3:
+#             return True
+#         return False
+#
+#     def print_state(self):
+#         print('Картошка {} сейчас {}'.format(
+#             self.index, Potato.state[self.state]
+#         ))
+#
+#
+# class PotatoGarden:
+#
+#     def __init__(self, count):
+#         self.potatoes = [Potato(index) for index in range(1, count + 1)]
+#
+#     def grow_all(self):
+#         print('Картошка прорастает.')
+#         for i_potato in self.potatoes:
+#             i_potato.grow()
+#
+#     def are_all_ripe(self):
+#         for i_potato in self.potatoes:
+#             if not i_potato.is_ripe():
+#                 print('Картошка ещё не созрела.\n')
+#                 break
+#         else:
+#             print('Картошка готова к сбору.\n')
+#
+#
+# my_garden = PotatoGarden(5)
+# my_garden.are_all_ripe()
+#
+# for _ in range(3):
+#     my_garden.grow_all()
+#     my_garden.are_all_ripe()
