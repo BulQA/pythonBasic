386e4bd6716590dcc82e7ff63cba967b6a54813b
Nikolay Garipov <kolya.garipov.2000@mail.ru>
2025-09-04 21:58:42 +0300

    Генераторы и их реализация

--- OOP_LVL/practice_3_1.py
+++ OOP_LVL/practice_3_1.py
@@ -170,3 +170,38 @@
 #
 #
 # print(7 in Fibonacci(10))
+
+# 13.5 Генераторы и их реализация
+
+# def Fibonacci(number):
+#     cur_val = 0
+#     next_val = 1
+#
+#     for _ in range(number):
+#         yield cur_val
+#         cur_val, next_val = next_val, cur_val + next_val
+#         if cur_val > 10**6:
+#             return
+#
+#
+# def square(nums):
+#     for num in nums:
+#         yield num ** 2
+#
+#
+# fibo = Fibonacci(number=1000000)
+#
+# for i in fibo:
+#     print(i, end=' ')
+# print('\n')
+#
+# # Генератор от генератора
+# print(sum(square(Fibonacci(number=1470))))
+#
+# # Генераторные выражения
+# print()
+# cubes_gen = (num ** 3 for num in range(10))
+#
+# for i_num in cubes_gen:
+#     print(i_num, end=' ')
+
