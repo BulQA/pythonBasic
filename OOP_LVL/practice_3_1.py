3a1913893bcea4dac4d31f39c8bde8b34ef3b3fc
Nikolay Garipov <kolya.garipov.2000@mail.ru>
2025-09-06 21:54:21 +0300

    Функция как объект. Функции высшего порядка

--- OOP_LVL/practice_3_1.py
+++ OOP_LVL/practice_3_1.py
@@ -236,3 +236,41 @@
 #         cur_val, next_val = next_val, cur_val + next_val
 #         if cur_val > 10 ** 6:
 #             return
+
+#14.2 Функция как объект. Функции высшего порядка
+
+# import time
+# from typing import Callable, Any
+#
+# def timer(func: Callable, *args, **kwargs) -> Any:
+#     '''Функция таймер'''
+#
+#     started_at = time.time()
+#     result = func(*args, **kwargs)
+#     ended_at = time.time()
+#
+#     run_time = round(ended_at - started_at, 4)
+#
+#     print('Функция работала {} секунд(ы)'.format(run_time))
+#     return result
+#
+# def squares_sum() -> int:
+#     number = 100
+#     result = 0
+#
+#     for _ in range(number + 1):
+#         result += sum([i_num ** 2 for i_num in range(1000)])
+#     return result
+#
+# def cubes_sum(number: int) -> int:
+#     result = 0
+#     for _ in range(number + 1):
+#         result += sum([i_num ** 3 for i_num in range(1000)])
+#     return result
+#
+#
+# new_result = timer(squares_sum)
+# print(new_result)
+#
+# new_cubes = timer(cubes_sum, 200)
+# print(new_cubes)
