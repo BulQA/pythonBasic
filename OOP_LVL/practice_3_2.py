346d0dac5acc74e2d19e6544162a01a6b358d4ef
Nikolay Garipov <kolya.garipov.2000@mail.ru>
2025-09-13 23:55:07 +0300

    Декораторы для классов

--- OOP_LVL/practice_3_2.py
+++ OOP_LVL/practice_3_2.py
@@ -263,8 +263,80 @@
 # new_cubes_sum = cubes_sum(number=300)
 # print(new_cubes_sum)
 
+#16.4 Декораторы для классов
 
-
+# import functools
+# from datetime import datetime
+# import time
+# from typing import Callable
+#
+#
+# def createtime(cls):
+#     """Декоратор класса, выводит время создания экземпляра класса."""
+#     @functools.wraps(cls)
+#     def wrapper(*args, **kwargs):
+#         instance = cls(*args, **kwargs)
+#         print('Время создания экземпляра класса:', datetime.utcnow())
+#         return instance
+#     return wrapper
+#
+#
+# def timer(func: Callable) -> Callable:
+#     """Декоратор, выводящий время работы функции."""
+#     @functools.wraps(func)
+#     def wrapped(*args, **kwargs):
+#         start = time.time()
+#         result = func(*args, **kwargs)
+#         end = time.time()
+#         print('Время работы функции:', end - start)
+#         return result
+#     return wrapped
+#
+#
+# def for_all_methods(decorator: Callable) -> Callable:
+#     """Декоратор класса.
+#     Получает другой декоратор и
+#     применяет его ко всем методам класса.
+#     """
+#     @functools.wraps(decorator)
+#     def decorate(cls):
+#         for method_name in dir(cls):
+#             if not method_name.startswith('__'):
+#                 cur_method = getattr(cls, method_name)
+#                 decorated_method = decorator(cur_method)
+#                 setattr(cls, method_name, decorated_method)
+#         return cls
+#     return decorate
+#
+#
+# @createtime
+# @for_all_methods(timer)
+# class Functions:
+#     def __init__(self, max_num: int) -> None:
+#         self.max_num = max_num
+#
+#     def squares_sum(self) -> int:
+#         number = 100
+#         result = 0
+#         for _ in range(number + 1):
+#             result += sum(i_num ** 2 for i_num in range(self.max_num))
+#         return result
+#
+#     def cubes_sum(self, number: int) -> int:
+#         result = 0
+#         for _ in range(number):
+#             result += sum(i_num ** 3 for i_num in range(self.max_num))
+#         return result
+#
+#
+# new_func_1 = Functions(max_num=100)
+# time.sleep(1)
+# new_func_1 = Functions(max_num=200)
+# time.sleep(1)
+# new_func_1 = Functions(max_num=300)
+#
+# new_func_1.squares_sum()
+# new_func_1.cubes_sum(number=1000)
 
 
 
