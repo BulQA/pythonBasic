97bea02a26d9d2afdad29322664b3c85ca0af14c
Nikolay Garipov <kolya.garipov.2000@mail.ru>
2025-09-16 21:24:11 +0300

    функция __name__

--- OOP_LVL/home_work_3_0.py
+++ OOP_LVL/home_work_3_0.py
@@ -260,3 +260,13 @@
 # print(id(anothe_new_obj))
 #
 # print(new_obj is anothe_new_obj)
+
+#18.1 Разбор домашнего задания
+
+# from collections import Counter
+#
+# def can_be_poly(val: str) -> bool:
+#     return len(list(filter(lambda x: x % 2, Counter(val).values()))) <= 2
+#
+# print(can_be_poly('ababc'))
+# print(can_be_poly('abbbc'))
---
+++ OOP_LVL/new_python.py
@@ -0,0 +1,9 @@
+# def func() -> None:
+#     print('Я функция func() из new_python.py')
+#
+#
+# if __name__ == '__main__':
+#     print('Здесь основной код')
+#     func()
+# else:
+#     print('Импортирован модуль', __name__)
