cd71946e1964366c42fa5c35e6bfd845fbe5e000
Nikolay Garipov <kolya.garipov.2000@mail.ru>
2025-10-20 19:43:17 +0300

    List comprehensions с условиями. Модуль random

--- Advanced_LVL/home_work_2_1.py
+++ Advanced_LVL/home_work_2_1.py
@@ -437,3 +437,17 @@
 # new_prices = [price if price > 0 else 0 for price in original_prices]
 #
 # print(new_prices)
+
+# `````````````````Дз```````````````````````#
+
+# import random
+#
+# team_1 = [random.randint(50, 80) for _ in range(10)]
+# team_2 = [random.randint(30, 60) for _ in range(10)]
+#
+#
+# team_3 = ['Погиб' if team_1[i] + team_2[i] > 100 else 'Выжил' for i in range(10)]
+#
+# print(f"Урон первого отряда: {team_1}")
+# print(f"Урон второго отряда: {team_2}")
+# print(f"Состояние третьего отряда: {team_3}")
