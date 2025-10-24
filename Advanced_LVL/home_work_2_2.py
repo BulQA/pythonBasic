622051e6a72d934db5807e628e65d8af5a465c87
Nikolay Garipov <kolya.garipov.2000@mail.ru>
2025-10-24 23:30:30 +0300

    4.7 Практическая работа

---
+++ Advanced_LVL/home_work_2_2.py
@@ -0,0 +1,17 @@
+#4.7 Практическая работа
+
+# text = input('Введите текст: ')
+# vowels = 'аоуэиыеёюяАОУЭИЫЕЁЮЯ'
+#
+# letters = [char.lower() for char in text if char.lower() in vowels]
+#
+# print('Список гласных букв:', letters)
+# print('Длина списка:', len(letters))
+
+# +++++++++++++++++++++++++++++++++++++++++++#
+
+# N = int(input('Введите длину списка: '))
+#
+# result = [1 if i % 2 == 0 else i % 5 for i in range(N)]
+#
+# print('Результат:', result)
