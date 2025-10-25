657fa15e50d18af8a5519451e3259bfc50c4967f
Nikolay Garipov <kolya.garipov.2000@mail.ru>
2025-10-25 23:26:56 +0300

    4.7 Практическая работа

--- Advanced_LVL/home_work_2_2.py
+++ Advanced_LVL/home_work_2_2.py
@@ -15,3 +15,22 @@
 # result = [1 if i % 2 == 0 else i % 5 for i in range(N)]
 #
 # print('Результат:', result)
+
+# +++++++++++++++++++++++++++++++++++++++++++#
+
+# import random
+#
+# team_1 = [round(random.uniform(5, 10), 2) for _ in range(20)]
+# team_2 = [round(random.uniform(5, 10), 2) for _ in range(20)]
+#
+# winners = []
+#
+# for i in range(20):
+#     if team_1[i] > team_2[i]:
+#         winners.append(team_1[i])
+#     else:
+#         winners.append(team_2[i])
+#
+# print("Первая команда:", team_1)
+# print("Вторая команда:", team_2)
+# print("Победители тура:", winners)
