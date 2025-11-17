99ad40decdcf184a81dd1c8a746b0e0b5b320400
Nikolay Garipov <kolya.garipov.2000@mail.ru>
2025-11-17 22:28:33 +0300

    6.2 Методы словарей

---
+++ Advanced_LVL/home_work_2_3.py
@@ -0,0 +1,40 @@
+#6.2 Методы словарей
+
+# small_storage = {
+#     'гвозди': 5000,
+#     'шурупы': 3040,
+#     'саморезы': 2000
+# }
+#
+# big_storage = {
+#     'доски': 1000,
+#     'балки': 150,
+#     'рейки': 600
+# }
+#
+# combined = [small_storage, big_storage]
+# big_storage = {}
+#
+# for d in combined :
+#     big_storage.update(d)
+#
+# order = input('Введите название товара:')
+#
+# if order in big_storage:
+#     print('На складе:',big_storage.get(order), order)
+# else:
+#     print('Такого товара нет на складе')
+
+# +++++++++++++++++++++++++++++++++++++++++++#
+
+# incomes = {
+#     'apple': 5600.20,
+#     'orange': 3500.45,
+#     'banana': 5000.00,
+#     'bergamot': 3700.56,
+#     'durian': 5987.23,
+#     'grapefruit': 300.40,
+#     'peach': 10000.50,
+#     'pear': 1020.00,
+#     'persimmon': 310.00,
+# }
